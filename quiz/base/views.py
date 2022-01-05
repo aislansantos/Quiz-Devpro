94d1bbb39d5c0ff41ae7b74fdc3bb1f46a782408
Aislan Santos <aislan_85@yahoo.com.br>
2022-01-04 22:18:16 -0300

    Inicio do Mapeamento

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse # importo o modulo HttpResponse que me da a resposta no meu mapeamento.
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request): #como funciona a requisução ?
+    return HttpResponse('Olá mundo!')
