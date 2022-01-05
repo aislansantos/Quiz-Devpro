1ac051e992e9ab5a3f3da8236021896f12ebe312
Aislan Santos <aislan_85@yahoo.com.br>
2022-01-05 20:55:36 -0300

    Inicialização de templates

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse # importo o modulo HttpResponse que me da a resposta no meu mapeamento.
 from django.shortcuts import render
 
 # Create your views here.
 
 
-def home(request): #como funciona a requisução ?
-    return HttpResponse('Olá mundo!')
+def home(request): #como funciona a requisução/request ?
+    return render(request, 'base/home.html')
