3c1954928a62c65dd755ad7ff41bce6936ced590
Aislan Santos <aislan_85@yahoo.com.br>
2022-01-08 16:54:45 -0300

    modelo criado para perguntas e informações de perguntas do jogo colocado na pagina do game

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+from quiz.base.models import Pergunta
 
 
-def home(request): #como funciona a requisução/request ?
+def home(request):  # como funciona a requisução/request ?
     return render(request, 'base/home.html')
+
+
+def classificacao(request):
+    return render(request, 'base/classificacao.html')
+
+
+def perguntas(request, indice):
+    pergunta = Pergunta.objects.filter(disponivel=True).order_by('id')[indice - 1]
+    context = {'indice_da_questao': indice, 'pergunta': pergunta}
+    return render(request, 'base/game.html', context=context)
